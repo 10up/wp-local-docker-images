cf2f953ce9dbc727b1e4fa25c3cc797f33b23641
Chris Marslender <chrismarslender@gmail.com>
2018-08-03 18:08:35 -0700

    Ensure that linux hosts also have an entry for host.docker.internal

--- phpfpm/5.5/entrypoint.sh
+++ phpfpm/5.5/entrypoint.sh
@@ -12,5 +12,18 @@ if [ ! $www_uid -eq 0 ]; then
 	groupmod -g $www_gid www-data
 fi
 
+# Ensure we have the host.docker.internal hostname available to linux as well
+function fix_linux_internal_host() {
+  DOCKER_INTERNAL_HOST="host.docker.internal"
+
+  if ! grep $DOCKER_INTERNAL_HOST /etc/hosts > /dev/null ; then
+    DOCKER_INTERNAL_IP=`/sbin/ip route|awk '/default/ { print $3 }'`
+    echo -e "$DOCKER_INTERNAL_IP\t$DOCKER_INTERNAL_HOST" | tee -a /etc/hosts > /dev/null
+    echo 'Added $DOCKER_INTERNAL_HOST to hosts /etc/hosts'
+  fi
+}
+
+fix_linux_internal_host
+
 exec "$@"
 
